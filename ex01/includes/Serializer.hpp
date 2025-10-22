6979238b6238099f3f91a537be818a5b6afd5d35
LouAntonio <lourencodala88@gmail.com>
2025-10-22 13:29:33 +0100

    basic config

---
+++ ex01/includes/Serializer.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 10:57:00 by lantonio          #+#    #+#             */
+/*   Updated: 2025/10/22 13:29:08 by lantonio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERIALIZER
+#define SERIALIZER
+
+#include <iostream>
+#include "./Data.hpp"
+
+class Serializer {
+	private:
+		Serializer();
+		Serializer(const Serializer &src);
+		Serializer &operator=(const Serializer &src);
+		virtual ~Serializer();
+
+	public:
+		static uintptr_t serialize(Data* ptr);
+		static Data* deserialize(uintptr_t raw);
+};
+
+#endif

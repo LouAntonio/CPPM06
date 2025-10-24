f929993358da3f748a387f041f19989e53b1cd3e
LouAntonio <lourencodala88@gmail.com>
2025-10-24 10:54:38 +0100

    identifying referecncies to identify classes and using empty exceptions in case of errors

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 11:39:33 by lantonio          #+#    #+#             */
-/*   Updated: 2025/10/23 15:11:11 by lantonio         ###   ########.fr       */
+/*   Updated: 2025/10/24 10:46:12 by lantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <exception>
 
 Base* generate(void) {
 	int debug = 0;
@@ -47,21 +48,32 @@ void identify(Base* p) {
 		std::cout << "B" << std::endl;
 	else if (dynamic_cast<C*>(p))
 		std::cout << "C" << std::endl;
+	else if (dynamic_cast<Base*>(p))
+		std::cout << "Base" << std::endl;
 	else
-		std::cout << "Desconhecido" << std::endl;
+		std::cout << "Unknown" << std::endl;
 }
 
 void	identify(Base& p) {
-	Base* ptr = &p;
-
-	if (dynamic_cast<A*>(ptr))
+	try {
+		dynamic_cast<A&>(p);
 		std::cout << "A" << std::endl;
-	else if (dynamic_cast<B*>(ptr))
+		return;
+	} catch (std::exception &e) { }
+
+	try {
+		dynamic_cast<B&>(p);
 		std::cout << "B" << std::endl;
-	else if (dynamic_cast<C*>(ptr))
+		return;
+	} catch (std::exception &e) { }
+
+	try {
+		dynamic_cast<C&>(p);
 		std::cout << "C" << std::endl;
-	else
-		std::cout << "Desconhecido" << std::endl;
+		return;
+	} catch (std::exception &e) { }
+
+	std::cout << "Base" << std::endl;
 }
 
 int main(void) {
